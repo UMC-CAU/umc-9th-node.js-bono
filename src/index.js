e8bfc1a56c6a7577c1f2ed42351daa17b6bb245d
kimbohyun <bhyn@kimbohyuns-MacBook-Air.local>
2025-11-12 20:33:40 +0900

    7주차 미션: 3. morgan 미들웨어 적용

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
+
 import { handleUserSignUp } from "./controllers/user.controller.js";
 import { handleStoreSignUp } from "./controllers/store.controller.js";
 import {
@@ -21,6 +23,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
+app.use(morgan("dev")); // 로그 포맷: dev
 app.use(cors()); // cors 방식 허용
 app.use(express.static("public")); // 정적 파일 접근
 app.use(express.json()); // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
