dbcf598882e7387e1834dd46c90a7703c7256f4c
kimbohyun <bhyn@kimbohyuns-MacBook-Air.local>
2025-11-20 21:13:22 +0900

    handleListStoreReviews 스웨거 추가

--- src/controllers/review.controller.ts
+++ src/controllers/review.controller.ts
@@ -15,6 +15,56 @@ export const handleReviewSignUp = async (req: any, res: any, next: any) => {
 };
 
 export const handleListStoreReviews = async (req: any, res: any, next: any) => {
+  /* 
+    #swagger.summary = '가게 리뷰 목록 조회 API';
+    #swagger.responses[200]={
+    description: "가게 리뷰 목록 조회 성공 응답",
+    content:{
+        "application/json":{
+            schema: {
+                type: "object",
+                properties:{
+                  resultType: { type: "string", example: "SUCCESS" },
+                  error:{
+                    type:"object", nullable:true, example:null},
+                  success:{
+                    type:"object",
+                    properties:{
+                      data:{ type:"array",
+                      items: {
+                        type: "object",
+                        properties: {
+                          id: { type: "number" },
+                          content: { type: "string" },
+                          store_id: { type: "number" },
+                          user_id: { type: "number" },
+                          created_at: { type: "string", format: "date-time" },
+                          rate: { type: "number" },
+                          store:{
+                            type: "object",
+                            properties:{
+                              name: { type: "string" },
+                            },
+                          },
+                          user:{
+                            type: "object",
+                            properties:{
+                              name: { type: "string" },
+                            },
+                          },
+                        }
+                      },
+                      pagination:{ type:"object", 
+                        properties:{
+                          cursor:{ type:"number"}
+                        }
+                      }
+                    } 
+                }
+            }
+        }
+    }
+  */
   console.log("가게 리뷰 목록 조회를 요청했습니다!");
   console.log("params:", req.params);
   console.log("query:", req.query);
